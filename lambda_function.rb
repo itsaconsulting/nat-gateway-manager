f37500b129155275fb77ac70f2c7534d4cf741af
Joshua Miller <joshua@itsecureadmin.com>
2024-09-24 09:08:11 -0700

    add functionality to associate and disassociate the EIP, as well as update source/destination check

--- lambda_function.rb
+++ lambda_function.rb
@@ -15,15 +15,25 @@ def lambda_handler(event:, context:)
 
   report_deployment_status(event:) unless event['DeploymentId'].nil?
 
-  # determine event type:  scale out, scale in
+  # on scale out, get eip, associate eip, disable source/dest check, update route table(s)
+  if event['detail-type'] == "EC2 Instance Launch Successful"
+    $logger.info("Associating an EIP to instance #{get_ec2_instance_id_from_event(event:)}")
+    eip_allocation_id = get_available_eip_allocation_id
+    if eip_allocation_id == 0
+      $logger.info("No free EIPs found.")
+      exit 1
+    end
+    associate_address(event:, eip_allocation_id:)
+    disable_source_destination_check(ec2_instance_id: get_ec2_instance_id_from_event(event:))
+#    add_entry_to_route_table(event:)
+  end
 
-  # on scale out, get eip, associate eip, disable source/dest check
-#  eip = get_available_eip
-#  associate_address
-#  disable_source_destination_check
-  
-  # on scale in, disassociate eip
-#  disassociate_address
+  # on scale in, disassociate eip, update route table(s)
+  if event['detail-type'] == "EC2 Instance Terminate Successful"
+    $logger.info("Disassociating the EIP from instance #{get_ec2_instance_id_from_event(event:)}")
+    disassociate_address(event:)
+#    remove_entry_from_route_table
+  end
 
 end
 
@@ -42,70 +52,66 @@ def report_deployment_status(event:)
   })
 end
 
-def get_available_eip()
+def get_available_eip_allocation_id()
+  resp = $client.describe_addresses({})
 
-  resp = client.describe_addresses({})
-#    filters: [
-#      {
-#        name: "String",
-#        values: ["String"],
-#      },
-#    ],
-#    public_ips: ["String"],
-#    allocation_ids: ["AllocationId"],
-#    dry_run: false,
-#  })
+  resp['addresses'].each do |address|
+    if address[:association_id].nil?
+      $logger.info("Found a free EIP,AllocationId: #{address[:public_ip]}, #{address[:allocation_id]}")
+      return address[:allocation_id]
+    end
+  end
+  return 0
+end
 
-#resp.to_h outputs the following:
-#{
-#  addresses: [
-#    {
-#      domain: "standard",
-#      instance_id: "i-1234567890abcdef0",
-#      public_ip: "198.51.100.0",
-#    },
-#    {
-#      allocation_id: "eipalloc-12345678",
-#      association_id: "eipassoc-12345678",
-#      domain: "vpc",
-#      instance_id: "i-1234567890abcdef0",
-#      network_interface_id: "eni-12345678",
-#      network_interface_owner_id: "123456789012",
-#      private_ip_address: "10.0.1.241",
-#      public_ip: "203.0.113.0",
-#    },
-#  ],
-#}
+def get_instance_eip_assocation_id(ec2_instance_id:)
+  resp = $client.describe_addresses({})
 
+  resp['addresses'].each do |address|
+    if address[:instance_id] == ec2_instance_id
+      $logger.info("Assocation ID for instance: #{address[:association_id]}, #{address[:instance_id]}")
+      return address[:association_id]
+    end
+  end
 end
 
 
-def associate_address()
 
-#resp = client.associate_address({
-#  allocation_id: "eipalloc-64d5890a", 
-#  instance_id: "i-0b263919b6498b123", 
-#})
-#
-#resp.to_h outputs the following:
-#{
-#  association_id: "eipassoc-2bebb745",
-#}
+def associate_address(event:, eip_allocation_id:)
+  $logger.info("Associating #{eip_allocation_id} to #{get_ec2_instance_id_from_event(event:)}")
 
+  association_id = resp = $client.associate_address({
+    allocation_id: eip_allocation_id,
+    instance_id: get_ec2_instance_id_from_event(event:),
+  })
+
+  $logger.info("Associated #{eip_allocation_id} to #{get_ec2_instance_id_from_event(event:)} with resulting assocation_id: #{association_id}.")
 end
   
-  
-def disassociate_address()
+def disassociate_address(event:)
+  $logger.info("Disassociating EIP from: #{get_ec2_instance_id_from_event(event:)}.")
+  association_id = get_instance_eip_assocation_id(ec2_instance_id: get_ec2_instance_id_from_event(event:))
 
-# disassociate an address on scale in
-# resp = client.disassociate_address({
-#   association_id: "eipassoc-2bebb745", 
-# })
-
+  resp = $client.disassociate_address({
+    association_id: association_id,
+  })
 end
 
-def disable_source_destination_check()
-
+def disable_source_destination_check(ec2_instance_id:)
+  $logger.info("Disabling source/destination check on: #{ec2_instance_id}.")
+  $client.modify_instance_attribute(
+    instance_id: ec2_instance_id,
+    source_dest_check: {
+      value: false,
+    }
+  )
 end
 
+def get_ec2_instance_id_from_event(event:)
+  if event['detail']['EC2InstanceId'].nil?
+    return 0
+  else
+    return event['detail']['EC2InstanceId']
+  end
+end
 
